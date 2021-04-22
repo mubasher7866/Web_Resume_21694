faae5e55948779352289a3a78bbe00377a452978
Mubasher <21694@student.dorset-college.ie>
2021-04-23 00:06:45 +0100

    init

---
+++ js/app.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+
+    $('#profile__ripple').ripples({
+        resolution: 512,
+        dropRadius: 10
+    });
+
+    const bars = document.querySelectorAll('.progress__bar');
+
+    bars.forEach(function (bar) {
+        let percentage = bar.dataset.percent;
+        let tooltip = bar.children[0];
+        tooltip.innerText = percentage + '%';
+        bar.style.width = percentage + '%';
+        console.log(percentage);
+    })
+
+    // Counter
+
+    const counters = document.querySelectorAll('.counter');
+    console.log(counters);
+
+    function runCounter() {
+        counters.forEach(counter => {
+            counter.innerText = 0;
+            let target = +counter.dataset.count;
+            let step = target / 100;
+            
+
+            let countIt = function () {
+                let displayedCount = +counter.innerText;
+                if (displayedCount < target) {
+                    counter.innerText = Math.ceil(displayedCount + step);
+                    setTimeout(countIt, 1);
+
+                } else {
+                    counter.innerText = target;
+                }
+            }
+            countIt();
+
+
+        })
+    }
+
+    
+
+    let counterSection = document.querySelector('.counter__section');
+
+    let options = {
+        rootMargin :'0px 0px -200px 0px'
+    }
+    let done  = 0;
+
+    const sectionObserver = new IntersectionObserver(function(entries){
+
+        if(entries[0].isIntersecting && done !== 1) {
+            done = 1;
+            runCounter();
+        }
+
+    }, options)
+
+    sectionObserver.observe(counterSection);
+
+
+    // image filter
+        var $wrapper = $('.portfolio__wrapper');
+
+});
